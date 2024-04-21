5e8c52c35968f1a90bffb152689eab0a7f4f3030
Linusx <57060126+linuzzx@users.noreply.github.com>
2024-04-21 09:36:52 +0200

    Aktualisieren von html_ne.py

--- generate_pages/html_ne.py
+++ generate_pages/html_ne.py
@@ -9,6 +9,7 @@ html = '''
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 <title>Birthday Counter</title>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 <style>
 /* general styling */
 :root {{
